798e1903180caf14d4fa884d6fc426e8873b9d60
yeongjae_lee <31798849+yeongjaelee@users.noreply.github.com>
2022-05-27 15:16:28 +0000

    third commit

--- project3/blog/urls.py
+++ project3/blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns =[
     path("lisbon/",views.Image_index,name="lisbon"),
     path("skills/",views.skills,name="skills"),
     path("contact/",views.contact,name="contact"),
-    path("post/<int:pk>",views.PostDetailView.as_view(),name="post")
-
+    path("post/<int:pk>",views.PostDetailView.as_view(),name="post"),
+    path("login/",views.login,name="login"),
+    path("signup/",views.signup,name="signup")
 ]
--- project3/blog/views.py
+++ project3/blog/views.py
@@ -3,8 +3,8 @@ from django import views
 from django.shortcuts import render,redirect
 from .models import Category,Post,Image
 from django.views import generic
-
-
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 def Image_index(request):
@@ -51,6 +51,35 @@ def contact(req):
     }
     return render(req,"contact.html",context=context)
 
+def login(req):
+    if req.method=="POST":
+        username = req.POST["username"]
+        password = req.POST["password"]
+        user = auth.authenticate(req, username=username, password=password)
+        if user is not None:
+            auth.login(req,user)
+            return redirect("index")
+        else:
+            return render(req,"login.html",{'error':'username or password is incorrect'})
+    
+    else:
+        return render(req,'login.html')
+
+def logout(req):
+    auth.logout(req)
+    return redirect("index")
+
+def signup(req):
+    if req.method =="POST":
+        if req.POST["password1"] == req.POST["password2"]:
+            user = User.objects.create_user(
+                username = req.POST["username"], password = req.POST["password1"])
+            auth.login(req,user)
+        return render(req,'signup.html')
+    
+    return render(req,'signup.html')
+
+                
 class PostDetailView(generic.DetailView):
     model = Post
 
